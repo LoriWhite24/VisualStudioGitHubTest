d4118ceb70df95fc53e4388432d55d60561242aa
Lori White <lori.r.white.24@gmail.com>
2020-08-17 17:50:00 -0400

    Added today's time

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -15,27 +15,43 @@ import * as serviceWorker from './serviceWorker';
     return <h1>Hello, Stranger.</h1>;
 }*/
 
-const user = {
+/*const user = {
     firstName: 'Harper',
     lastName: 'Perez',
     avatarUrl: "https://newevolutiondesigns.com/images/freebies/cool-wallpaper-1.jpg"
-};
+};*/
 
-const element = (
+/*const element = (
     <div>
         <h1>Hello!</h1>
         <h2>Good to see you here.</h2>
         <img className="photo" src={user.avatarUrl} alt={"img not found"}></img>
     </div>
-    );
+    );*/
 
-ReactDOM.render(
-  /*<React.StrictMode>
+
+
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+
+
+
+/*ReactDOM.render(
+  *//*<React.StrictMode>
     <App />
-  </React.StrictMode>*/
+  </React.StrictMode>*//*
     element,
   document.getElementById('root')
-);
+);*/
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
