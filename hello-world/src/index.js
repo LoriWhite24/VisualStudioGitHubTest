7c2816084bc4bbe8dcfd641ffca9c5c8029b3104
Lori White <lori.r.white.24@gmail.com>
2020-08-17 16:55:41 -0400

    Changed Greeting and added img

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+/*function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}*/
+
+/*function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+}*/
+
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez',
+    avatarUrl: "https://newevolutiondesigns.com/images/freebies/cool-wallpaper-1.jpg"
+};
+
+const element = (
+    <div>
+        <h1>Hello!</h1>
+        <h2>Good to see you here.</h2>
+        <img className="photo" src={user.avatarUrl} alt={"img not found"}></img>
+    </div>
+    );
+
 ReactDOM.render(
-  <React.StrictMode>
-        <div class="container"><h1>Hello World!</h1></div>
-        <App />
-  </React.StrictMode>,
+  /*<React.StrictMode>
+    <App />
+  </React.StrictMode>*/
+    element,
   document.getElementById('root')
 );
 
